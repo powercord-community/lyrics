abf96d18a0aaaf881c9a9a7f219c91b4192c94cb
David Ralph <ohlookitsderpy@protonmail.com>
2020-01-25 16:48:15 +0000

    Make pc-spotify optional (untested)

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const { get }                = require('powercord/http');
-const { Plugin }             = require('powercord/entities');
-const SpotifyPlayer          = require('../pc-spotify/SpotifyPlayer.js');
+//* Imports
+const { get } = require('powercord/http');
+const { Plugin } = require('powercord/entities');
 const { messages, channels } = require('powercord/webpack');
+// Try to import pc-spotify
+let SpotifyPlayer;
+try {
+  SpotifyPlayer = require('../pc-spotify/SpotifyPlayer.js');
+} catch (e) {
+  SpotifyPlayer = null;
+}
 
+//* Export
 module.exports = class Lyrics extends Plugin {
   startPlugin () {
     this.registerCommand(
@@ -12,7 +20,10 @@ module.exports = class Lyrics extends Plugin {
       'lyrics <song || ???>',
       async ([ args ]) => {
         try {
-          let data = await get(`https://lyrics-api.powercord.dev/lyrics?input=${args || SpotifyPlayer.player.item.name + SpotifyPlayer.player.item.artists[0].name}`).then(res => res.body);
+          // Get data...
+          let data;
+          if (SpotifyPlayer) data = await get(`https://lyrics-api.powercord.dev/lyrics?input=${args || SpotifyPlayer.player.item.name + SpotifyPlayer.player.item.artists[0].name}`).then(res => res.body);
+          else data = await get(`https://lyrics-api.powercord.dev/lyrics?input=${args}`).then(res => res.body);
           if (!data.data[0].lyrics) {
             return {
               send: false,
@@ -21,7 +32,8 @@ module.exports = class Lyrics extends Plugin {
           }
           data = `${data.data[0].artist} - ${data.data[0].name}\n\n${data.data[0].lyrics}\n\nLyrics provided by KSoft.Si | © ${data.data[0].artist} ${data.data[0].album_year}`;
           const value = data.replace(/(?:\\[rn])+/g, '');
-          if (value.length > 2000) {
+          // ...then send it
+          if (value.length > 2000) { // Send to user if it's over 2000 characters
             return {
               send: false,
               result: `\`\`\`${value}\`\`\``
